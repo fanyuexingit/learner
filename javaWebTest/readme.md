c6645c3107f4f178e532a94a98fcda5ec4ed43e2
fanyuexin <13581558845@163.com>
2021-02-01 07:32:09 +0800

    servlet

--- javaWebTest/readme.md
+++
@@ -1,3 +0,0 @@
-# 项目：javaWeb 项目
-
-# 作用：javaWeb 相关知识点
---
+++ javaWebTest/src/main/java/com/fan/servlet/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.fan.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Hello,World");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
